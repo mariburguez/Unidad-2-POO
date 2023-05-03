ec2f37679a9565212a15140a930bf65369497e37
mariburguez <burguezmarisol@gmail.com>
2023-05-03 16:07:33 -0300

    first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,48 @@
+print("Practico 2 - Ejercicio 1")
+
+class email:
+    #ATRIBITOS DE LA CLASS"
+    idCuenta= ""
+    dominio= "nada"
+    tipoDominio= "nada"
+    contraseña="nada"
+    
+    #metodo constructor
+    def _init_(self, idCuenta, dominio, tipoDominio, contraseña):
+        self.idCuenta= idCuenta
+        self.dominio=dominio 
+        self.tipoDominio=tipoDominio
+        self.contraseña=contraseña
+
+    #Método “retornaEmail()” 
+    def retornaEmail(self, idCuenta, dominio, tipoDominio):
+        self.idCuenta= idCuenta
+        self.dominio=dominio
+        self.tipoDominio=tipoDominio
+        nuevoEmail= idCuenta+"@"+dominio+"."+tipoDominio
+        return nuevoEmail
+    #retorna dominio: 
+    def getDominio():
+        return self.dominio
+    
+    #crear cuenta
+    def crearCuenta(self):
+        self.idCuenta=input("Dime tu idCuenta: ")
+        self.dominio=input("Dime tu dominio: ")
+        self.tipoDominio=input("Dime tu tipoDominio: ")
+        self.tipoDominio=input("Dime tu contraseña: ")
+
+    if __name__=='__main__':
+        #no se como instanciar (diapo 34)
+        email1=email()
+        #ingresa por tecado los datos
+        idCuenta=input("Dime tu idCuenta: ")
+        dominio=input("Dime tu dominio: ")
+        tipoDominio=input("Dime tu tipoDominio: ")
+        #llamadas:
+        email1.retornaEmail(self, idCuenta, dominio, tipoDominio)
+
+        #print("el dominio es: " self.dominio)
+
+        email1.crearCuenta()
+    
